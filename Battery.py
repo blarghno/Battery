636d7d75b3480b682d896644ccc61b992123b185
blarghnog <55301229+blarghnog@users.noreply.github.com>
2019-12-30 15:59:03 -0500

    Final commit(practicing)

--- Battery.py
+++ Battery.py
@@ -13,14 +13,14 @@ class Battery:
     AREA_OF_SENSOR = 0.15 #in square millimeters
 
     #constructor for battery
-    def __init__(self, fileName):
-        self.readConfigFile(fileName)
-    
-    #constructor for battery without reading from config file
-    def setProperties(self, currentCapacity, maxCapacity, load):
-        self.currentCapacity = currentCapacity
-        self.maxCapacity = maxCapacity
-        self.load = load
+    #def __init__(self, fileName):
+     #   self.readConfigFile(fileName)
+        
+    #constructor for battery without reading from config file    
+    def __init__(self, currentCapacity, maxCapacity, load):
+        self.__currentCapacity = currentCapacity
+        self.__maxCapacity = maxCapacity
+        self.__load = load
     
     #reads from config file and sets the values to the object attributes   
     def readConfigFile(self, fileName):
@@ -59,7 +59,7 @@ class Battery:
         self.__maxCapacity = value
     
     def __str__(self):
-        return (f'Max Capacity: {self.__maxCapacity} J\nCurrent Capacity: {self.__currentCapacity} J')
+        return (f'Max Capacity: {self.__maxCapacity} J\nCurrent Capacity: {self.__currentCapacity} J\nLoad: {self.__load}')
         
     def __repr__(self):
         return (f'{self.__class__.__name__}({self.__energyHarvested!r}')
@@ -86,50 +86,66 @@ class Battery:
     #86370 = 1day
     #reads the file from a range of line numbers
     def readFileRange(self, fileName, start, end):
+        f = open(fileName, "r")
         inc = 0
         fail = 0
-        t = []
-        d = []
-        f = open(fileName, "r")
         lasttime = 0
         time = 0
+        time_list = []
+        capacity_list = []
+        
         for i in range(start, end):
-        #for i in f:
             data = lc.getline(fileName, i).split()
             lasttime = time
             time = int(data[0])
             irr = float(data[1])
             energy = self.convert(irr, self.AREA_OF_SENSOR, time-lasttime)
-            q = self.loadBattery(energy, self.__load * (time-lasttime))
-            if (q == 1):
+            inc_or_fail = self.loadBattery(energy, self.__load * (time-lasttime))
+            if (inc_or_fail == 1):
                 inc+=1
-            if (q == -1):
+            if (inc_or_fail == -1):
                 fail+=1
-            t.append(time)
-            d.append(self.__currentCapacity)
-            #data.append(lc.getline(fileName, i).split()[1])
-            #print(self.__currentCapacity)
+            time_list.append(time)
+            capacity_list.append(self.__currentCapacity)    
         f.close()
-        #print(t)
-        #print(d)
-        #print(len(t), len(d))
-        t2 = [t[i] for i in range(len(t)) if i % 3600 == 0]
-        d2 = [d[i] for i in range(len(d)) if i % 3600 == 0]
+        
+        time_axis = [time_list[i] for i in range(len(time_list)) if i % 3600 == 0]
+        capacity_axis = [capacity_list[i] for i in range(len(capacity_list)) if i % 3600 == 0]
         print("Stored",inc,"times out of",end-start,"% is",inc/(end-start))
         print("Failed",fail,"times out of",end-start,"% is",fail/(end-start))
-        plt.scatter(t2, d2)
-        plt.show()
-
-    #def readFileEntire(self, fileName) 
+        plt.scatter(time_axis, capacity_axis)
+        plt.xlabel('Time(sec)')
+        plt.ylabel('Battery Capacity(J)')
+        
+        graph_name = input("input filename\n")
+        plt.savefig(graph_name)
+ 
+        plt.show()        
+        
+        '''save = input("save? y or n")
+        
+        if(save == "y"):
+            graph_name = input()
+            plt.savefig(graph_name)
+        else:
+            print('oof')'''
     
-    #will convert irradiance to mW-h?
+    #convert irradiance to Joules. 
     def convert(self, irrad, area, time):
         return irrad * area * time
     
-battery = Battery("battery_Config.txt")
-#arbitrary values
-print(battery)
-#irradiance data from line 2 to 100
-battery.readFileRange("SetupC.txt", 1 , 1000001)
+battery100 = Battery(0, 1000, 0.001)
+battery500 = Battery(0, 1000, 0.005)
+battery1000 = Battery(0, 1000, 0.01)
+battery2500 = Battery(0, 1000, 0.05)
+
+print(battery100)
+battery100.readFileRange("SetupD.txt", 2 , 900002)
+print(battery500)
+battery500.readFileRange("SetupD.txt", 2 , 900002)
+print(battery1000)
+battery1000.readFileRange("SetupD.txt", 2 , 900002)
+print(battery2500)
+battery2500.readFileRange("SetupD.txt", 2 , 900002)
 
 
